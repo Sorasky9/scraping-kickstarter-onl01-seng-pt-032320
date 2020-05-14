8e7e35221a73747c6c9cf237e8ef2340a922e876
Malcom Fetus <sora.vista@gmail.com>
2020-05-14 20:26:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,7 +20,7 @@ kickstarer.css("li.project.grid_4").each do |project|
   #^Up here we are using the projectS hash to call on the PROJECT elements from the iteration
 end
 
-projects
+projects #<- We have to call the projects hash from above to return the scraping elements
   binding.pry
   # write your code here
 end

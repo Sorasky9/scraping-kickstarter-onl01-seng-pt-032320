56cf2db2f2898807cd195c699565c1f4bdcc9fed
Malcom Fetus <sora.vista@gmail.com>
2020-05-14 21:05:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,7 @@ projects = {} #<-here we create an empty hash as it will contain the scraped ele
 kickstarer.css("li.project.grid_4").each do |project|
   title = project.css("h2.bbcard_name strong a").text
 
-  projects[title.to_sym] = { :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  projects[title.to_sym] = {:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
   :description => project.css("p.bbcard_blurb").text, :location => project.css("ul.project-meta span.location-name").text,
 :percent_funded => roject.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
   #^Up here we are using the projects hash with the title elements being converted to symbols
